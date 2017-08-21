de9ebf757487b52d378f996a2b1bdb258329ffad
Tarang Dave <tarangadave@gmail.com>
2017-08-21 12:18:14 +0530

    login design, front page setup done

--- src/components/frontpage.js
+++ src/components/frontpage.js
@@ -2,10 +2,42 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 class WelcomePage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: '',
+		};
+		this.editUsername = this.editUsername.bind(this);
+	}
+
+	editUsername(event) {
+		this.setState({username: event.target.value});
+	}
+
   render() {
     return (
       <div>
-      Hello World!!!
+      	<div className="frontpage-display-layout">
+      		<div className="frontpage-input-background col-lg-4 col-md-4 col-sm-4">
+      			<div className="frontpage-input-header">
+      			</div>
+      			<div className="row frontpage-input-content margin-0">
+      				<div className="all-font frontpage-input-title">Enter your chat name</div>
+      				<div className="col-lg-12 col-md-12 col-sm-12 frontpage-input-image-logo">
+      					<div className="col-lg-6 col-md-6 col-sm-6 ">
+      						<div>
+      							<img src="https://appbase.io/images/logo-blue.png" alt="Appbase Logo"/>
+      						</div>
+      					</div>
+      					<div className="col-lg-6 col-md-6 col-sm-6">
+      						<input type="text" value={this.state.username} onChange={this.editUsername} className="frontpage-input-style all-font" placeholder="Enter username"/>
+      					</div>
+      				</div>
+      			</div>
+      			<div className="frontpage-input-footer">
+      			</div>
+      		</div>
+      	</div>
       </div>
     );
   }
